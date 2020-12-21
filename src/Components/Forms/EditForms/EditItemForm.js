9c7b846c5b5616689a152f9458e5f3d458c1d65d
artwilton <Arthur.M.Wilton@gmail.com>
2020-12-21 01:40:49 -0500

    updated Android Manifest to include "<uses-sdk tools:overrideLibrary="com.imagepicker" to allow minimum sdk of 16

--- src/Components/Forms/EditForms/EditItemForm.js
+++ src/Components/Forms/EditForms/EditItemForm.js
@@ -1,8 +1,16 @@
 import React, {Component} from 'react';
 
 import {Picker} from '@react-native-picker/picker';
-import { Text, TextInput, View, Keyboard, TouchableOpacity } from 'react-native';
-import { styles } from  '../../../Styles'
+import {
+  Text,
+  TextInput,
+  View,
+  Keyboard,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
+import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
+import {styles} from '../../../Styles';
 
 class EditItemForm extends Component {
   state = {
@@ -12,13 +20,32 @@ class EditItemForm extends Component {
     notes: '',
     barcode: '',
     selected_container: {},
-    selected_category: {}
+    selected_category: {},
   };
 
   componentDidMount() {
-    console.log('clicked obj', this.props.clickedObj)
-    let { id, name, description, notes, barcode, container, category } = this.props.clickedObj
-    this.setState({id, name, description, notes, barcode, selected_container: container, selected_category: category}, ()=> console.log('mounted state', this.state))
+    console.log('clicked obj', this.props.clickedObj);
+    let {
+      id,
+      name,
+      description,
+      notes,
+      barcode,
+      container,
+      category,
+    } = this.props.clickedObj;
+    this.setState(
+      {
+        id,
+        name,
+        description,
+        notes,
+        barcode,
+        selected_container: container,
+        selected_category: category,
+      },
+      () => console.log('mounted state', this.state),
+    );
   }
 
   editItemFormHandler = (text, name) => {
@@ -28,18 +55,24 @@ class EditItemForm extends Component {
   renderContainerValues = () => {
     return this.props.containers.map((obj) => (
       <Picker.Item key={obj.id} label={obj.name} value={obj} />
-  ));
-  }
+    ));
+  };
 
   renderCategoryValues = () => {
     return this.props.categories.map((obj) => (
       <Picker.Item key={obj.id} label={obj.name} value={obj} />
-  ));
-  }
+    ));
+  };
 
   render() {
     return (
       <View>
+        <Image
+          style={this.props.style.fullSizePhoto}
+          source={{
+            uri: `http://10.0.2.2:3000${this.props.clickedObj.photos[0]}`,
+          }}
+        />
         <TextInput
           onChangeText={(text) => this.editItemFormHandler(text, 'name')}
           placeholder={'Item Name'}
@@ -65,7 +98,9 @@ class EditItemForm extends Component {
           selectedValue={this.state.selected_container}
           style={{height: 50, width: 300}}
           onValueChange={(itemValue, itemIndex) =>
-            this.setState({selected_container: itemValue}, ()=> console.log('containers', this.state.selected_container))
+            this.setState({selected_container: itemValue}, () =>
+              console.log('containers', this.state.selected_container),
+            )
           }>
           {this.renderContainerValues()}
         </Picker>
@@ -78,8 +113,10 @@ class EditItemForm extends Component {
           }>
           {this.renderCategoryValues()}
         </Picker>
-        <TouchableOpacity onPress={() => this.props.editItem(this.state)} style={styles.button}>
-            <Text>Save Changes</Text>
+        <TouchableOpacity
+          onPress={() => this.props.editItem(this.state)}
+          style={styles.button}>
+          <Text>Save Changes</Text>
         </TouchableOpacity>
       </View>
     );
